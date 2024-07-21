be85d6380878eab18cc117d86a88ae9187bdade5
Dharini Panchal 1191207 <d_panchal209666@fanshaweonline.ca>
2024-07-21 00:50:11 -0400

    Tab Navigator - Transaction and Summary created with icons

--- App.js
+++ App.js
@@ -1,14 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const TransactionListScreen = () => (
+  <View style={styles.container}>
+    <Text>Transaction List</Text>
+  </View>
+);
+
+const SummaryScreen = () => (
+  <View style={styles.container}>
+    <Text>Summary</Text>
+  </View>
+);
+
+const Stack = createStackNavigator();
+
+const TransactionStackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="TransactionList" component={TransactionListScreen} />
+  </Stack.Navigator>
+);
+
+const Tab = createBottomTabNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Transactions') {
+            iconName = focused ? 'list' : 'list-outline';
+          } else if (route.name === 'Summary') {
+            iconName = focused ? 'information' : 'information-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'blue',
+        inactiveTintColor: 'grey',
+      }}
+    >
+
+      <Tab.Screen name="Transactions" component={TransactionStackNavigator} />
+      <Tab.Screen name="Summary" component={SummaryScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +65,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
